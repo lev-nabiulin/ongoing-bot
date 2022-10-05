b24e6a8b15005ad6335e22e9c39c9d8654a13d2c
Lev Nabiulin <levnabiulin@yandex.ru>
2022-10-05 15:40:33 +0500

    OOP paradigma levvas'a

--- parse-logic/parse-testing.py
+++ parse-logic/parse-testing.py
@@ -2,23 +2,33 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 # Подключить бд.
-# Нужно бахнуть по ООП, с классами и всей херней.
-
 
 urls = [
     "https://animejoy.ru/tv-serialy/2891-vremya-nindzya.html",
     "http://rutor.info/torrent/890745/nadvoe-01-02-iz-09-2022-web-dl-1080p",
 ]
 
-for url in urls:
-    r = requests.get(url)
-    soup = bs(r.text, "html.parser")
-    if "animejoy" in url:
-        blocks = soup.find_all("div", class_="titleup")
-        for block in blocks:
-            title = block.find("h1", class_="h2 ntitle").get_text()
-            print(title)
 
-    if "rutor" in url:
-        name = soup.find("h1").get_text()
-        print(name)
+class Parser:
+    def animejoy_parse(urls):
+        for url in urls:
+            r = requests.get(url)
+            soup = bs(r.text, "html.parser")
+            if "animejoy" in url:
+                blocks = soup.find_all("div", class_="titleup")
+                for block in blocks:
+                    animejoy_title = block.find("h1", class_="h2 ntitle").get_text()
+                    return animejoy_title
+
+    def rutor_parse(urls):
+        for url in urls:
+            r = requests.get(url)
+            soup = bs(r.text, "html.parser")
+            if "rutor" in url:
+                rutor_title = soup.find("h1").get_text()
+                return rutor_title
+
+
+if __name__ == "__main__":
+    print(Parser.animejoy_parse(urls))
+    print(Parser.rutor_parse(urls))
