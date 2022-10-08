f566e3e97366b6c675aa46bbe1169268f8bf55f3
supremko <supremko.fun@gmail.com>
2022-10-08 22:23:21 +0300

    order creating

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -7,6 +7,12 @@ type text not null check(name='webviewer' and name='tracker'),
 unique (name, url)
 );
 
+create table users (
+id integer primary key autoincrement not null,
+tg_id integer not null unique,
+admin integer not null check(admin=1 and admin=0) default 0
+);
+
 create table subscriptions (
 id integer primary key autoincrement not null,
 user_id integer not null,
@@ -27,12 +33,6 @@ ep_now integer,
 ep_total integer,
 last_check_date integer,
 FOREIGN KEY(res_id) REFERENCES resources(id)
-);
-
-create table users (
-id integer primary key autoincrement not null,
-tg_id integer not null unique,
-admin integer not null check(admin=1 and admin=0) default 0
 );
 
 create table shikimori (
