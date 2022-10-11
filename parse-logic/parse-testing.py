4c096960bdc8696eb8e38837b2a38be3b68ea4fa
Lev Nabiulin <levnabiulin@yandex.ru>
2022-10-11 17:26:39 +0500

    added rutor_year

--- parse-logic/parse-testing.py
+++ parse-logic/parse-testing.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as bs
+from parsel import Selector
 
 # Подключить бд.
 
@@ -8,12 +9,14 @@ urls = [
     "http://rutor.info/torrent/890745/nadvoe-01-02-iz-09-2022-web-dl-1080p",
 ]
 
+
 class UrlTest:
     def url_is_valid(urls):
         for url in urls:
             r = requests.get(url)
             r_ok = r.ok
             return r_ok
+
 
 class Parser:
     def animejoy_parse(urls):
@@ -24,17 +27,23 @@ class Parser:
                 blocks = soup.find_all("div", class_="titleup")
                 for block in blocks:
                     animejoy_title = block.find("h1", class_="h2 ntitle").get_text()
-                    if animejoy_title not in '':
+                    if animejoy_title not in "":
                         return animejoy_title
 
     def rutor_parse(urls):
+
         for url in urls:
             r = requests.get(url)
             soup = bs(r.text, "html.parser")
+            text_s = requests.get(url).text
+            sel = Selector(text=text_s)
             if "rutor" in url:
                 rutor_title = soup.find("h1").get_text()
-                if rutor_title not in '':    
-                    return rutor_title
+                rutor_year = sel.xpath(
+                    '//b[starts-with(text(),"Год выхода")]/following-sibling::text()'
+                ).get()
+                if rutor_title not in "":
+                    return rutor_title, rutor_year
 
 
 if __name__ == "__main__":
